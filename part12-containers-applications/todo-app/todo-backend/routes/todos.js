1731cb2abb6a2d80f30c4b7717160110eb58e085
Valtteri Andström <valtteri.andstrom@gmail.com>
2021-10-25 23:22:55 +0300

    Ex 6-7 done

--- part12-containers-applications/todo-app/todo-backend/routes/todos.js
+++ part12-containers-applications/todo-app/todo-backend/routes/todos.js
@@ -5,6 +5,8 @@ const router = express.Router();
 /* GET todos listing. */
 router.get('/', async (_, res) => {
   const todos = await Todo.find({})
+  console.log(todos)
+  console.log('aaa')
   res.send(todos);
 });
 
@@ -20,8 +22,10 @@ router.post('/', async (req, res) => {
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
+
   const { id } = req.params
   req.todo = await Todo.findById(id)
+ 
   if (!req.todo) return res.sendStatus(404)
 
   next()
@@ -35,12 +39,25 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo); // Implement this
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  console.log('KYRPÄ!!!!')
+  const newTodo = {
+    text: req.todo.text,
+    done: true
+  }
+  console.log('TÄMÄ ', req.todo.id)
+
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(req.todo.id, newTodo, {new: true})
+    res.send(updatedTodo);
+  } catch(exception) {
+    res.sendStatus(404)
+  }
+ // Implement this
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
